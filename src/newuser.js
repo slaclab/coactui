c837701d845340f1e2d1117d190a17cf075651b3
Murali Shankar <mshankar@slac.stanford.edu>
2022-06-30 13:46:59 -0700

    Request user account.
    Also, formik and yup for validation

--- src/newuser.js
+++ src/newuser.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { Component, useState } from 'react';
+import _ from "lodash";
 import Container from 'react-bootstrap/Container';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import { useMutation, gql } from "@apollo/client";
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
 const REQUEST_USERACCOUNT_MUTATION = gql`
 mutation newSDFAccountRequest($request: SDFRequestInput!){
@@ -14,6 +18,90 @@ mutation newSDFAccountRequest($request: SDFRequestInput!){
 }
 `;
 
+const schema = yup.object().shape({
+  userName: yup.string().required().min(5, "Usernames must be 5 characters or more").max(15, "Usernames cannot be more than 15 characters"),
+});
+
+function FormExample(props) {
+  return (
+    <Formik
+      validationSchema={schema}
+      onSubmit={values => { console.log("Submitting"); alert(JSON.stringify(values, null, 2)); props.handleSubmit(); }}
+      initialValues={{
+        userName: '',
+      }}
+      validateOnChange={true}
+      validateOnBlur={true}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        touched,
+        isValid,
+        errors,
+      }) => (
+        <Form noValidate onSubmit={handleSubmit}>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationFormik01" className="position-relative">
+              <Form.Label>Preferred username</Form.Label>
+              <Form.Control
+                type="text"
+                name="userName"
+                value={values.userName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isValid={touched.userName && !errors.userName}
+                isInvalid={touched.userName && errors.userName}
+              />
+              <Form.Control.Feedback type='invalid' tooltip>{errors.userName}</Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Button type="submit">Submit form</Button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+class ReqUserAccount extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { preferredUserName: _.split(this.props.eppn, "@")[0], userNameInvalid: false }
+    this.handleClose = () => { this.props.setShow(false); }
+    this.requestAccount = () => {
+      console.log(this.state.preferredUserName);
+      if(_.isEmpty(this.state.preferredUserName)) {
+        this.setState({ userNameInvalid: true });
+        return;
+      }
+      this.props.requestUserAccount(this.state.preferredUserName);
+      this.props.setShow(false);
+    }
+    this.setPreferredUserid = (event) => { this.setState({ preferredUserName: event.target.value }) }
+  }
+  render() {
+    return (
+      <Modal show={this.props.show} onHide={this.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Request an SDF account</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Request an SDF account for {this.props.eppn}. Please choose a username.
+          <FormExample handleSubmit={this.requestAccount}/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={this.requestAccount}>
+            Request Account
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      )
+  }
+}
 
 export default function NewUser(props) {
   const [ requestUserAccount, { uudata, uuloading, uuerror }] = useMutation(REQUEST_USERACCOUNT_MUTATION);
@@ -48,20 +136,7 @@ export default function NewUser(props) {
         If you would like to request an account, please click here.
        </div>
        <button type="button" className="btn btn-primary" onClick={handleShow}>Request an SDF account</button>
-       <Modal show={show} onHide={handleClose}>
-         <Modal.Header closeButton>
-           <Modal.Title>Request an SDF account</Modal.Title>
-         </Modal.Header>
-         <Modal.Body>Request an SDF account for {props.eppn}</Modal.Body>
-         <Modal.Footer>
-           <Button variant="secondary" onClick={handleClose}>
-             Close
-           </Button>
-           <Button variant="primary" onClick={requestAccount}>
-             Request Account
-           </Button>
-         </Modal.Footer>
-         </Modal>
+       <ReqUserAccount show={show} setShow={setShow} eppn={props.eppn} requestUserAccount={requestAccount} />
        </Container>
     </>
   );
