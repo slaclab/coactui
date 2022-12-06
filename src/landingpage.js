03e20c8c7afc626a0abb3d12e3e8210aed9be057
yee379 <yee379@gmail.com>
2022-12-06 13:26:53 -0800

    feat: add buttons for login

--- src/landingpage.js
+++ src/landingpage.js
@@ -2,35 +2,33 @@ import Container from 'react-bootstrap/Container';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGlobe, faRobot  } from '@fortawesome/free-solid-svg-icons'
-import { Footer } from "./tabs/widgets";
-
 
 export default function LandingPage() {
   return (<Container fluid id="landingpage">
     <div className="content">
       <Row>
         <Col>
-          <Card>
+          <Card className="text-center">
             <Card.Body>
-              <Card.Title>Register</Card.Title>
-              <Card.Text>Register to use S3DF resources</Card.Text>
               <div><FontAwesomeIcon icon={faGlobe} size="6x" className="cardicn text-center"/></div>
+              <Card.Text>Register to use S3DF resources</Card.Text>
+              <Button variant="primary" size="lg" href="/register">Register...</Button>
             </Card.Body>
           </Card>
         </Col>
         <Col>
-          <Card>
+          <Card className="text-center">
             <Card.Body>
-              <Card.Title>Login</Card.Title>
-              <Card.Text>Login to use Coact to manage your S3DF resources</Card.Text>
               <div><FontAwesomeIcon icon={faRobot} size="6x" className="cardicn text-center"/></div>
+              <Card.Text>Login to use Coact to manage your S3DF resources</Card.Text>
+              <Button variant="secondary" size="lg" href="/login">Login...</Button>
             </Card.Body>
           </Card>
         </Col>
       </Row>
     </div>
-    <Footer/>
   </Container>);
 }
