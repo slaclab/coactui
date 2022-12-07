bdda1e6da8871021d89e11076d99d65a3fed752e
Murali Shankar <mshankar@slac.stanford.edu>
2022-12-07 15:03:39 -0800

    Switch to the post registration page after submitting the registration request

--- src/register.js
+++ src/register.js
@@ -15,11 +15,14 @@ import { Formik } from 'formik';
 import * as yup from 'yup';
 
 
-const FACNREPOS = gql`
-query allreposandfacility {
-  allreposandfacility {
-    name
-    facility
+const FACNAMES = gql`
+query facilityNames {
+  facilityNames
+  amIRegistered {
+    isRegistered
+    isRegistrationPending
+    eppn
+    fullname
   }
 }
 `;
@@ -71,7 +74,7 @@ function UserIdForm(props) {
                 <Form.Label>Which facility will you using?</Form.Label>
                 <Form.Select name="facility" value={values.facility} isValid={touched.facility && !errors.facility} isInvalid={touched.facility && errors.facility} onChange={(e) => { values.facility = e.target.value; props.handleFacility(values.facility)}} onBlur={handleBlur}>
                   <option value="">Please select a facility</option>
-                  { _.map(_.uniq(_.map(props.repos, "facility")), (f) => { return (<option key={f} value={f}>{f}</option>)}) }
+                  { _.map(props.facilityNames, (f) => { return (<option key={f} value={f}>{f}</option>)}) }
                 </Form.Select>
                 <Form.Control.Feedback type='invalid' tooltip>{errors.facility}</Form.Control.Feedback>
               </Form.Group>
@@ -104,22 +107,17 @@ class ReqUserAccount extends Component {
         <Modal.Header closeButton>
           <Modal.Title>Request an SDF account</Modal.Title>
         </Modal.Header>
-        <UserIdForm handleSubmit={this.requestAccount} preferredUserName={_.split(this.props.eppn, "@")[0]} handleClose={this.handleClose} eppn={this.props.eppn} repos={this.props.repos} handleFacility={this.props.handleFacility} selectedFacility={this.props.selectedFacility}/>
+        <UserIdForm handleSubmit={this.requestAccount} preferredUserName={_.split(this.props.eppn, "@")[0]} handleClose={this.handleClose} eppn={this.props.eppn} facilityNames={this.props.facilityNames} handleFacility={this.props.handleFacility} selectedFacility={this.props.selectedFacility}/>
       </Modal>
       )
   }
 }
 
 export default function RegisterUser(props) {
-  const { loading, error, data } = useQuery(FACNREPOS);
+  const { loading, error, data } = useQuery(FACNAMES);
   const [ requestUserAccount, { uudata, uuloading, uuerror }] = useMutation(REQUEST_USERACCOUNT_MUTATION);
   const [show, setShow] = useState(false);
   const [facility, setFacility] = useState("");
-  const requestAccount = (preferredUserName) => {
-    console.log("Account requested for eppn " + props.eppn + " and preferred userid " + preferredUserName);
-    requestUserAccount({ variables: { request: { reqtype: "UserAccount", eppn: props.eppn, preferredUserName: preferredUserName, "facilityname": facility }}});
-    setShow(false);
-  };
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
@@ -132,20 +130,32 @@ export default function RegisterUser(props) {
   if (error) return <p>Error :</p>;
   console.log(data)
 
-  if(props.isRegistered) {
+  let hasUserAcc = _.get(data, "amIRegistered.isRegistered", false);
+  let eppn = _.get(data, "amIRegistered.eppn", null);
+  let registrationPending = _.get(data, "amIRegistered.isRegistrationPending", false);
+  let isRegistered = _.get(data, "amIRegistered.isRegistered", false);
+  let fullname = _.get(data, "amIRegistered.fullname", "");
+
+  const requestAccount = (preferredUserName) => {
+    console.log("Account requested for eppn " + eppn + " and preferred userid " + preferredUserName);
+    requestUserAccount({ variables: { request: { reqtype: "UserAccount", eppn: eppn, preferredUserName: preferredUserName, "facilityname": facility }}, refetchQueries: [ FACNAMES, 'amIRegistered' ]});
+    setShow(false);
+  };
+
+  if(isRegistered) {
     return (
       <Navigate to="../myprofile" replace />
     );
   }
 
-  if (props.registrationPending) {
+  if (registrationPending) {
     return (
       <>
       <div className="registeruser d-flex flex-column">
          <NoNavHeader/>
-         <h6 className="p-2">Hi <span className="text-primary">{props.fullname}</span>, welcome to Coact; the portal for using the SDF.</h6>
+         <h6 className="p-2">Hi <span className="text-primary">{fullname}</span>, welcome to Coact; the portal for using the SDF.</h6>
          <div className="p-2 flex-grow-1">
-          Your request to enable the EPPN <span className="text-primary"><b>{props.eppn}</b></span> for SDF is still pending and will be acted upon soon.
+          Your request to enable the EPPN <span className="text-primary"><b>{eppn}</b></span> for SDF is still pending and will be acted upon soon.
          </div>
          <Footer/>
       </div>
@@ -157,13 +167,13 @@ export default function RegisterUser(props) {
     <>
     <div className="registeruser d-flex flex-column">
       <NoNavHeader/>
-      <h6 className="p-2">Hi <span className="text-primary">{props.fullname}</span>, welcome to Coact; the portal for using the SDF.</h6>
+      <h6 className="p-2">Hi <span className="text-primary">{fullname}</span>, welcome to Coact; the portal for using the SDF.</h6>
       <div className="p-2 flex-grow-1">
-        You don't seem to have an SDF account associated with your EPPN <span className="text-primary"><b>{props.eppn}</b></span>.
+        You don't seem to have an SDF account associated with your EPPN <span className="text-primary"><b>{eppn}</b></span>.
         If you would like to request your account be enabled for SDF, please click here -
         <button type="button" className="btn btn-primary" onClick={handleShow}>Enable my SDF account</button>
       </div>
-      <ReqUserAccount show={show} setShow={setShow} eppn={props.eppn} requestUserAccount={requestAccount} repos={data.allreposandfacility} handleFacility={handleFacility} selectedFacility={facility} />
+      <ReqUserAccount show={show} setShow={setShow} eppn={eppn} requestUserAccount={requestAccount} facilityNames={data.facilityNames} handleFacility={handleFacility} selectedFacility={facility} />
       <Footer/>
     </div>
     </>
