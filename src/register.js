d2f9c62f72ed8fdc3788d956ef9400e8efe4bf4a
Murali Shankar <mshankar@slac.stanford.edu>
2022-12-07 17:01:20 -0800

    Remove Formik from register page

--- src/register.js
+++ src/register.js
@@ -6,14 +6,12 @@ import Col from "react-bootstrap/Col";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import { Navigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRocket, faPerson, faPersonCircleQuestion } from '@fortawesome/free-solid-svg-icons'
 import { NoNavHeader, Footer } from "./tabs/widgets";
 import { useQuery, useMutation, gql } from "@apollo/client";
-import { Formik } from 'formik';
-import * as yup from 'yup';
-
 
 const FACNAMES = gql`
 query facilityNames {
@@ -36,78 +34,46 @@ mutation newSDFAccountRequest($request: SDFRequestInput!){
 }
 `;
 
-const schema = yup.object().shape({
-  userName: yup.string().required().min(5, "Usernames must be 5 characters or more").max(15, "Usernames cannot be more than 15 characters"),
-  facility: yup.string().required()
-});
-
-function UserIdForm(props) {
-  return (
-    <Formik
-      validationSchema={schema}
-      onSubmit={values => { props.handleSubmit(values.userName); }}
-      initialValues={{
-        userName: props.preferredUserName,
-        facility: props.selectedFacility
-      }}
-      validateOnChange={true}
-      validateOnBlur={true}
-    >
-      {({
-        handleSubmit,
-        handleChange,
-        handleBlur,
-        values,
-        touched,
-        isValid,
-        errors,
-      }) => (
-        <Form noValidate onSubmit={handleSubmit}>
-          <Modal.Body>Enable your EPPN <span className="text-primary"><b>{props.eppn}</b></span> for the SDF.
-            <Row className="mt-3">
-              <Form.Group as={Col} md="4" controlId="validationFormik01" className="position-relative">
-                <Form.Label>Username: <span className="text-primary"><b>{values.userName}</b></span></Form.Label>
-              </Form.Group>
-            </Row>
-            <Row className="mb-3">
-              <Form.Group as={Col} md="4" controlId="validationFormik02" className="position-relative">
-                <Form.Label>Which facility will you using?</Form.Label>
-                <Form.Select name="facility" value={values.facility} isValid={touched.facility && !errors.facility} isInvalid={touched.facility && errors.facility} onChange={(e) => { values.facility = e.target.value; props.handleFacility(values.facility)}} onBlur={handleBlur}>
-                  <option value="">Please select a facility</option>
-                  { _.map(props.facilityNames, (f) => { return (<option key={f} value={f}>{f}</option>)}) }
-                </Form.Select>
-                <Form.Control.Feedback type='invalid' tooltip>{errors.facility}</Form.Control.Feedback>
-              </Form.Group>
-            </Row>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={props.handleClose}>Close</Button>
-            <Button type="submit">Request Account</Button>
-          </Modal.Footer>
-
-        </Form>
-      )}
-    </Formik>
-  );
-}
-
 class ReqUserAccount extends Component {
   constructor(props) {
     super(props);
     this.handleClose = () => { this.props.setShow(false); }
-    this.requestAccount = (preferredUserName) => {
-      this.props.requestUserAccount(preferredUserName);
+    this.requestAccount = () => {
+      const selFac = this.state.facility;
+      if(_.isEmpty(selFac) || !_.includes(this.props.facilityNames, selFac)) {
+        this.setState({facilityInvalid: true});
+        return;
+      }
+
+      this.props.requestUserAccount(selFac);
       this.props.setShow(false);
     }
-    this.setPreferredUserid = (event) => { this.setState({ preferredUserName: event.target.value }) }
+    this.state = { facility: "", facilityInvalid: false }
+    this.setFacility = (event) => { this.setState({ facility: event.target.value }); }
   }
+
   render() {
     return (
       <Modal show={this.props.show} onHide={this.handleClose} size="lg">
         <Modal.Header closeButton>
           <Modal.Title>Request an SDF account</Modal.Title>
         </Modal.Header>
-        <UserIdForm handleSubmit={this.requestAccount} preferredUserName={_.split(this.props.eppn, "@")[0]} handleClose={this.handleClose} eppn={this.props.eppn} facilityNames={this.props.facilityNames} handleFacility={this.props.handleFacility} selectedFacility={this.props.selectedFacility}/>
+        <Modal.Body>
+          <Row className="mb-3">
+            <Form.Text>Enable your EPPN <span className="text-primary"><b>{this.props.eppn}</b></span> for the SDF</Form.Text>
+            <InputGroup hasValidation>
+              <Form.Select name="facility" onChange={this.setFacility} isInvalid={this.state.facilityInvalid}>
+                <option value="">Please choose a facility</option>
+                { _.map(this.props.facilityNames, (f) => { return (<option key={f} value={f}>{f}</option>)}) }
+              </Form.Select>
+              <Form.Control.Feedback type="invalid">Please choose a valid facility.</Form.Control.Feedback>
+            </InputGroup>
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.props.handleClose}>Close</Button>
+          <Button type="submit" onClick={this.requestAccount} >Request Account</Button>
+        </Modal.Footer>
       </Modal>
       )
   }
@@ -117,14 +83,8 @@ export default function RegisterUser(props) {
   const { loading, error, data } = useQuery(FACNAMES);
   const [ requestUserAccount, { uudata, uuloading, uuerror }] = useMutation(REQUEST_USERACCOUNT_MUTATION);
   const [show, setShow] = useState(false);
-  const [facility, setFacility] = useState("");
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const handleFacility = (selectedFacilty) => {
-    setFacility(selectedFacilty);
-    console.log("Setting facility to " + selectedFacilty);
-  }
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :</p>;
@@ -136,9 +96,10 @@ export default function RegisterUser(props) {
   let isRegistered = _.get(data, "amIRegistered.isRegistered", false);
   let fullname = _.get(data, "amIRegistered.fullname", "");
 
-  const requestAccount = (preferredUserName) => {
-    console.log("Account requested for eppn " + eppn + " and preferred userid " + preferredUserName);
-    requestUserAccount({ variables: { request: { reqtype: "UserAccount", eppn: eppn, preferredUserName: preferredUserName, "facilityname": facility }}, refetchQueries: [ FACNAMES, 'amIRegistered' ]});
+  const requestAccount = (selectedFacility) => {
+    const preferredUserName = _.split(eppn, "@")[0];
+    console.log("Account requested for eppn " + eppn + " in facility "  + selectedFacility + " with preferred username " + preferredUserName);
+    requestUserAccount({ variables: { request: { reqtype: "UserAccount", eppn: eppn, preferredUserName: preferredUserName, "facilityname": selectedFacility }}, refetchQueries: [ FACNAMES, 'amIRegistered' ]});
     setShow(false);
   };
 
@@ -173,7 +134,7 @@ export default function RegisterUser(props) {
         If you would like to request your account be enabled for SDF, please click here -
         <button type="button" className="btn btn-primary" onClick={handleShow}>Enable my SDF account</button>
       </div>
-      <ReqUserAccount show={show} setShow={setShow} eppn={eppn} requestUserAccount={requestAccount} facilityNames={data.facilityNames} handleFacility={handleFacility} selectedFacility={facility} />
+      <ReqUserAccount show={show} setShow={setShow} eppn={eppn} requestUserAccount={requestAccount} facilityNames={data.facilityNames} />
       <Footer/>
     </div>
     </>
