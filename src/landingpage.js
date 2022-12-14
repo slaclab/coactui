a2ac05287b3cbdc716de853f842810d33ef2c562
yee379 <yee379@gmail.com>
2022-12-14 09:30:56 -0800

    feat: change landing page logo to rocket

--- src/landingpage.js
+++ src/landingpage.js
@@ -3,7 +3,7 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGlobe, faRobot  } from '@fortawesome/free-solid-svg-icons'
+import { faRocket  } from '@fortawesome/free-solid-svg-icons'
 
 export default function LandingPage() {
   return (
@@ -12,16 +12,7 @@ export default function LandingPage() {
         <Col>
           <Card className="text-center">
             <Card.Body>
-              <div><FontAwesomeIcon icon={faGlobe} size="6x" className="cardicn text-center"/></div>
-              <Card.Text>Register to use S3DF resources</Card.Text>
-              <Button variant="primary" size="lg" href="/register">Register...</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card className="text-center">
-            <Card.Body>
-              <div><FontAwesomeIcon icon={faRobot} size="6x" className="cardicn text-center"/></div>
+              <div><FontAwesomeIcon icon={faRocket} size="6x" className="cardicn text-center"/></div>
               <Card.Text>Login to use Coact to manage your S3DF resources</Card.Text>
               <Button variant="secondary" size="lg" href="/login">Login...</Button>
             </Card.Body>
