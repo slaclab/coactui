32f497e7923b9e6d3b4dd973bfa54483f4ff63dd
Murali Shankar <mshankar@slac.stanford.edu>
2023-10-17 20:08:25 -0700

    Add new repo compute allocations

--- src/tabs/reposcompute.js
+++ src/tabs/reposcompute.js
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import { NavLink } from "react-router-dom";
+import { NavLink, useOutletContext } from "react-router-dom";
 import { useQuery, useMutation, gql } from "@apollo/client";
 import React, { Component, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -51,6 +51,9 @@ query{
       clustername
     }
   }
+  clusters {
+    name
+  }
 }`;
 
 const ALLOCATION_REQUEST = gql`
@@ -67,6 +70,59 @@ mutation requestApprove($Id: String!){
 }
 `;
 
+class AddComputeAllocation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentCluster: "", facilityPurchased: 0.0, currentAllocation: 0, showalloc: false }
+    this.setCluster = (event) => { 
+      let clustername = event.target.value;
+      let facilityData = _.find(this.props.facilities, ["name", this.props.facilityname]);
+      let facilityPurchased = _.get(_.find(_.get(facilityData, "computepurchases", {}), ["clustername", clustername]), "purchased", 0.0);
+      this.setState({currentCluster: clustername, facilityPurchased: facilityPurchased, showalloc: true}) 
+    }
+    this.setAllocation = (event) => { this.setState({currentAllocation: event.target.value}) }
+    this.hideModal = () => { 
+      this.setState({currentCluster: "", facilityPurchased: 0.0, currentAllocation: 0, showalloc: false});
+      this.props.hideModal();
+    }
+  }
+
+  render() {
+    return (
+      <Modal show={this.props.showModal} onHide={() => {this.hideModal()}}>
+        <ModalHeader closeButton={true}>
+          <ModalTitle>Allocate some compute for the repo <b className="em">{this.props.reponame}</b> on a new cluster</ModalTitle>
+        </ModalHeader>
+        <ModalBody>
+        <InputGroup>
+            <InputGroup.Text>Choose a cluster:</InputGroup.Text>
+            <Form.Select name="cluster" value={this.state.currentCluster} onChange={this.setCluster}>
+                <option value="">Please choose a cluster</option>
+                { _.map(this.props.clustersunallocated, (s) => { return (<option key={s} value={s}>{s}</option>)}) }
+            </Form.Select>
+          </InputGroup>
+          <div className={this.state.showalloc ? "py-2" : "d-none"}>
+            <p>A compute allocation is specified as a percentage of the facility's allocation.</p>
+            <p>The facility <b className="em">{this.props.facilityname}</b> has purchased <b className="em">{this.state.facilityPurchased}</b> on the <b className="em">{this.props.clustername}</b> cluster</p>
+            <InputGroup hasValidation>
+              <InputGroup.Text>Current Percent:</InputGroup.Text>
+              <Form.Control type="number" onBlur={this.setAllocation} isInvalid={this.props.isError} defaultValue={this.props.currentAllocation}/>
+              <Form.Control.Feedback type="invalid">{this.props.errorMessage}</Form.Control.Feedback>
+            </InputGroup>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={() => {this.hideModal()}}>
+            Close
+          </Button>
+          <Button onClick={() => { this.props.applyNewAllocation(this.state.currentCluster, this.state.currentAllocation) }}>
+            Done
+          </Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
 
 class UpdateComputeAllocation extends Component {
   constructor(props) {
@@ -114,6 +170,7 @@ class ReposRows extends Component {
   render() {
     var first = true;
     let cas = _.get(this.props.repo, "currentComputeAllocations", [{}]), rows = cas.length;
+    if(cas.length == 0) { cas = [{"clustername": "N/A"}] }
     let trs = _.map(cas, (a) => {
       let facilityPurchased = _.get(_.find(this.facilityData.computepurchases, ["clustername", a.clustername]), "purchased", 0.0);
       let totalAllocatedCompute = _.get(a, "percentOfFacility", 0.0)*facilityPurchased/100.0;
@@ -122,18 +179,18 @@ class ReposRows extends Component {
         first = false;
         return (
           <tr key={this.facility+this.reponame+a.clustername} data-name={this.reponame}>
-            <td rowSpan={rows} className="vmid">{this.reponame}</td>
+            <td rowSpan={rows} className="vmid">{this.reponame} {this.props.canEditAllocations ? <span className="px-2 text-warning" title="Allocate compute on a new cluster" onClick={() => { this.props.showAddModal(this.props.repo, facilityPurchased) }}><FontAwesomeIcon icon={faPlus}/></span> : <span></span>}</td>
             <td rowSpan={rows} className="vmid">{this.props.repo.facilityObj.name}</td>
             <td rowSpan={rows} className="vmid">{this.props.repo.principal}</td>
-            <td><NavLink to={"/repos/compute/"+this.props.repo.facility+"/"+this.reponame+"/allocation/"+a.Id} key={this.reponame}>{a.clustername}</NavLink></td>
-            <td>{totalAllocatedCompute} {this.props.canEditAllocations ? <span className="px-2 text-warning" title="Edit allocated amount" onClick={() => { this.props.showUpdateModal(this.props.repo.facilityObj.name, this.reponame, a.clustername, a.start, _.get(a, "percentOfFacility", 0.0), facilityPurchased) }}><FontAwesomeIcon icon={faEdit}/></span> : <span></span>}</td>
+            <td>{a.clustername == "N/A" ? "None" : <NavLink to={"/repos/compute/"+this.props.repo.facility+"/"+this.reponame+"/allocation/"+a.Id} key={this.reponame}>{a.clustername}</NavLink>}</td>
+            <td>{totalAllocatedCompute} {this.props.canEditAllocations && a.clustername != "N/A" ? <span className="px-2 text-warning" title="Edit allocated amount" onClick={() => { this.props.showUpdateModal(this.props.repo, a, facilityPurchased) }}><FontAwesomeIcon icon={faEdit}/></span> : <span></span>}</td>
             <td><TwoPrecFloat value={totalUsedHours}/></td>
           </tr>)
         } else {
           return (
             <tr key={this.facility+this.reponame+a.clustername} data-name={this.reponame}>
               <td><NavLink to={"/repos/compute/"+this.props.repo.facility+"/"+this.reponame+"/allocation/"+a.Id} key={this.reponame}>{a.clustername}</NavLink></td>
-              <td>{totalAllocatedCompute} {this.props.canEditAllocations ? <span className="px-2 text-warning" title="Edit allocated amount" onClick={() => { this.props.showUpdateModal(this.props.repo.facilityObj.name, this.reponame, a.clustername, a.start, _.get(a, "percentOfFacility", 0.0), facilityPurchased) }}><FontAwesomeIcon icon={faEdit}/></span> : <span></span>}</td>
+              <td>{totalAllocatedCompute} {this.props.canEditAllocations ? <span className="px-2 text-warning" title="Edit allocated amount" onClick={() => { this.props.showUpdateModal(this.props.repo, a, facilityPurchased) }}><FontAwesomeIcon icon={faEdit}/></span> : <span></span>}</td>
               <td><TwoPrecFloat value={totalUsedHours}/></td>
             </tr>)
         }
@@ -146,9 +203,36 @@ class ReposRows extends Component {
 class ReposTable extends Component {
   constructor(props) {
     super(props);
-    this.state = { repo: "", facility: "", cluster: "", allocationStartTime: "", currentAllocation: 0, facilityPurchased: 0, showAddModal: false, showUpdateModal: false, updateModalClusterName: "", updateModalCurrentPurchase: 0, modalError: false, modalErrorMessage: "", showToast: false, toastMsg: ""};
-    this.showUpdateModal = (facility, repo, cluster, allocationStartTime, curalloc, facilityPurchased) => { this.setState({repo: repo, facility: facility, cluster: cluster, allocationStartTime: allocationStartTime, currentAllocation: curalloc, facilityPurchased: facilityPurchased, showAddModal: false, showUpdateModal: true, updateModalClusterName: "", updateModalCurrentPurchase: 0, modalError: false, modalErrorMessage: "", showToast: false, toastMsg: ""}) }
-    this.hideUpdateModal = () => { this.setState({repo: "", facility: "", cluster: "", allocationStartTime: "", currentAllocation: 0, facilityPurchased: 0, showAddModal: false, showUpdateModal: false, updateModalClusterName: "", updateModalCurrentPurchase: 0, modalError: false, modalErrorMessage: "", showToast: false, toastMsg: ""}) }
+    this.resetState = () => { return  { repo: "", facility: "", cluster: "", allocationStartTime: "", currentAllocation: 0, clustersunallocated: [], facilityPurchased: 0, showAddModal: false, showUpdateModal: false, updateModalClusterName: "", updateModalCurrentPurchase: 0, modalError: false, modalErrorMessage: "", showToast: false, toastMsg: ""}}
+    this.state = this.resetState();
+
+    this.showUpdateModal = (repoObj, compAlloc, facilityPurchased) => { 
+      this.setState((prevState) => {
+        let ret = this.resetState();
+        ret.repo = repoObj.name;
+        ret.facility = repoObj.facility;
+        ret.cluster = compAlloc.clustername;
+        ret.allocationStartTime = compAlloc.start;
+        ret.currentAllocation = _.get(compAlloc, "percentOfFacility", 0.0);
+        ret.facilityPurchased = facilityPurchased;
+        ret.showUpdateModal = true;
+        return ret;
+      })
+    }
+    this.hideUpdateModal = () => { this.setState((prevState) => { return this.resetState()}) }
+    this.showAddModal = (repoObj, facilityPurchased) => {
+      this.setState((prevState) => {
+        let ret = this.resetState();
+        ret.repo = repoObj.name;
+        ret.facility = repoObj.facility;
+        ret.facilityPurchased = facilityPurchased;
+        ret.clustersunallocated = _.difference(_.map(this.props.clusters, "name"), _.map(_.get(repoObj, "currentComputeAllocations"), "clustername", []));
+        ret.allocationStartTime = (new Date()).toISOString();
+        ret.showAddModal = true;
+        return ret;
+      })
+  }
+    
     this.applyUpdateAllocation = (curalloc) => { 
       if(curalloc < 0.0) {
         this.setState({modalError: true, modalErrorMessage: "Please enter a number >= 0"});
@@ -161,7 +245,21 @@ class ReposTable extends Component {
         }, 
         (errormsg) => { this.setState({modalError: true, modalErrorMessage: errormsg})} );
     }
+
+    this.applyNewAllocation = (clustername, curalloc) => { 
+      if(curalloc < 0.0) {
+        this.setState({modalError: true, modalErrorMessage: "Please enter a number >= 0"});
+        return;
+      }
+      this.props.actuallyChangeAllocation(this.state.facility, this.state.repo, clustername, this.state.allocationStartTime, curalloc, 
+        () => { 
+          this.hideUpdateModal();
+          this.setState({showToast: true, toastMsg: "A request to update the compute allocation has been made and approved. It will take a few seconds for this approval to be processed. Please refresh the screen after a little while to update the UI."})
+        }, 
+        (errormsg) => { this.setState({modalError: true, modalErrorMessage: errormsg})} );
+    }
   }
+
   render() {
     return (
       <>
@@ -173,10 +271,11 @@ class ReposTable extends Component {
           <thead>
             <tr><th>Repo name</th><th>Facility</th><th>PI</th><th>ClusterName</th><th>Total compute allocation</th><th>Total compute used</th></tr>
           </thead>
-          { _.map(this.props.repos, (r) => { return (<ReposRows key={r.facility+"_"+r.name} repo={r} facilities={this.props.facilities} canEditAllocations={this.props.canEditAllocations} showUpdateModal={this.showUpdateModal} />) }) }
+          { _.map(this.props.repos, (r) => { return (<ReposRows key={r.facility+"_"+r.name} repo={r} facilities={this.props.facilities} canEditAllocations={this.props.canEditAllocations} showUpdateModal={this.showUpdateModal} showAddModal={this.showAddModal}/>) }) }
           </table>
         </div>
         <UpdateComputeAllocation reponame={this.state.repo} facilityname={this.state.facility} clustername={this.state.cluster} currentAllocation={this.state.currentAllocation} facilityPurchased={this.state.facilityPurchased} showModal={this.state.showUpdateModal} showUpdateModal={this.showUpdateModal} hideModal={this.hideUpdateModal} isError={this.state.modalError} errorMessage={this.state.modalErrorMessage} applyUpdateAllocation={this.applyUpdateAllocation}/>
+        <AddComputeAllocation reponame={this.state.repo} facilityname={this.state.facility} facilities={this.props.facilities} clustersunallocated={this.state.clustersunallocated} showModal={this.state.showAddModal} hideModal={this.hideUpdateModal} isError={this.state.modalError} errorMessage={this.state.modalErrorMessage} applyNewAllocation={this.applyNewAllocation}/>
       </>
      )
   }
@@ -186,6 +285,8 @@ export default function ReposComputeListView() {
   const { loading, error, data, refetch } = useQuery(REPOS);
   const [ allocreq ] = useMutation(ALLOCATION_REQUEST);
   const [ requestApproveMutation ] = useMutation(APPROVE_REQUEST_MUTATION);
+  const [ toolbaritems, setToolbaritems ] = useOutletContext();
+
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :</p>;
@@ -211,7 +312,9 @@ export default function ReposComputeListView() {
 
   return (
     <>
-    <ReposTable repos={data.myRepos} facilities={data.facilities} canEditAllocations={canEditAllocations} actuallyChangeAllocation={actuallyChangeAllocation}/>
+    <ReposTable repos={data.myRepos} facilities={data.facilities} clusters={data.clusters} canEditAllocations={canEditAllocations} 
+      actuallyChangeAllocation={actuallyChangeAllocation}
+      toolbaritems={toolbaritems} setToolbaritems={setToolbaritems}/>
     </>
   );
 }
